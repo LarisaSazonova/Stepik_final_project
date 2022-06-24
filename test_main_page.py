adc48f1f717a1869f7367a1936ae4ca1f2189d94
Larisa <ssnewh@gmail.com>
2022-06-24 11:14:53 +0300

    created class LoginPage and tests for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -7,10 +8,17 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     
-    
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()    
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link) 
+    page.open()       
+    page.should_be_login_page()
+
     
+
