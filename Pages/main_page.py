0c9011c4f0a9a30c56b6c36d28d6b1aec9fa8753
Larisa <ssnewh@gmail.com>
2022-06-24 11:33:41 +0300

    edited test_guest_can_go_to_login_page

--- Pages/main_page.py
+++ Pages/main_page.py
@@ -2,6 +2,7 @@ from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
     
